ecfc6b3be09fe15af8e315f24404754ce5ef4482
simon tang <simont.codes@gmail.com>
2024-02-17 14:47:27 -0500

    finished projects page, added some styling to app and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
-import Navbar from "./components/Navbar"
-import { Home, About, Projects, Contact } from "./pages"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home, About, Projects, Contact } from "./pages";
 
 const App = () => {
   return (
-  <main className=" bg-slate-300/20 h-[100vh]">
-    <Router>
+    <main className=" bg-slate-300/20 h-full]">
+      <Router>
         <Navbar />
         <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/projects" element={<Projects/>} />
-            <Route path="/contact" element={<Contact/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-    </Router>
-  </main>
-  )
-}
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
